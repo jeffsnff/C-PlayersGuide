80e2bffdf295fa33909a03c917f0a004f87ab50c
Jeffrey Seneff <jeffsnff@gmail.com>
2025-01-02 08:29:38 -0500

    Adds ability for user to set Manticore Range between and including 0 and 100

--- HuntingManticore/Program.cs
+++ HuntingManticore/Program.cs
@@ -4,7 +4,23 @@ namespace HuntingManticore
 {
   internal class Program{
     static void Main(string[] args){
-      Console.WriteLine("Lets Begin");
+      int round = 1;
+      int cityHealth = 15;
+      int manticoreHealth = 10;
+      int manticoreRange = 0;
+      
+
+      // This could be a function
+      while(true){
+        int minRange = 0;
+        int maxRange = 100;
+        Console.Write("PLayer 1, how far away from the city do you want to station the Manticore? ");
+        manticoreRange = Convert.ToInt32(Console.ReadLine());
+        if(manticoreRange < minRange || manticoreRange > maxRange){
+          continue;
+        }
+        break;
+      }
     }
   }
 }
