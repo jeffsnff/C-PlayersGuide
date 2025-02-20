c55b878d61013b36f96daa1fc940065174ccf01e
Jeffrey Seneff <jeffsnff@gmail.com>
2025-02-20 18:07:56 -0500

    Finished The Password Validator

---
+++ PracticeLand/The_Password_Validator/PasswordValidator.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace The_Password_Validator
+{
+  internal class PasswordValidator
+  {
+    private string password;
+
+    public PasswordValidator(string password)
+    {
+      this.password = password;
+    }
+
+    public Boolean Validator()
+    {
+      Boolean capitalLetter = false;
+      Boolean lowerCaseLetter = false;
+      Boolean numberChar = false;
+      Boolean noCapitalT = true;
+      Boolean noAmpersand = true;
+
+      if (this.password.Length > 13 || this.password.Length < 6)
+      {
+        return false;
+      }
+
+      foreach (char letter in this.password)
+      {
+        if (letter.Equals('&'))
+        {
+          noAmpersand = false;
+          break;
+        }
+        if (letter.Equals('T'))
+        {
+          noCapitalT = false;
+          break;
+        }
+        if (char.IsUpper(letter))
+        {
+          capitalLetter = true;
+        }
+        if (char.IsLower(letter))
+        {
+          lowerCaseLetter = true;
+        }
+        if (char.IsNumber(letter))
+        {
+          numberChar = true;
+        }
+      }
+
+      if (
+          capitalLetter == true &&
+          lowerCaseLetter == true &&
+          numberChar == true &&
+          noCapitalT == true &&
+          noAmpersand == true
+         )
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
---
+++ PracticeLand/The_Password_Validator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.VisualBasic;
+
+namespace The_Password_Validator
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      while (true)
+      {
+        Console.Clear();
+        Console.Write("Type a Password: ");
+        string userPassword = Console.ReadLine();
+        PasswordValidator validatePassword = new PasswordValidator(userPassword);
+        Console.WriteLine(validatePassword.Validator());
+        Console.ReadKey();
+      }
+    }
+  }
+}
